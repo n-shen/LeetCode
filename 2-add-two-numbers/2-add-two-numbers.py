bd96d13415da1cbd46bfbb2bf17ec22988d4bbf6
Nicholas Shen <n@shen.dev>
2022-04-18 21:26:23 -0400

    Time: 66 ms (93.95%), Space: 13.8 MB (88.62%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -10,28 +10,28 @@ class Solution:
         carry = (l1.val + l2.val) // 10 # carry over
         cur_node = digit_sum
         
-        while l1.next and l2.next:
-            l1 = l1.next
-            l2 = l2.next
-            cur_node.next = ListNode(val = (l1.val + l2.val + carry) % 10)
-            carry = (l1.val + l2.val + carry) // 10
-            cur_node = cur_node.next
-            
-        while l1.next:
-            l1 = l1.next
-            cur_node.next = ListNode(val = (l1.val + carry) % 10)
-            carry = (l1.val + carry) // 10
-            cur_node = cur_node.next
-            
-        while l2.next:
-            l2 = l2.next
-            cur_node.next = ListNode(val = (l2.val + carry) % 10)
-            carry = (l2.val + carry) // 10
-            cur_node = cur_node.next
-        
+        while l1.next or l2.next:
+            if l1.next and l2.next:
+                l1 = l1.next
+                l2 = l2.next
+                cur_node.next = ListNode(val = (l1.val + l2.val + carry) % 10)
+                carry = (l1.val + l2.val + carry) // 10
+                cur_node = cur_node.next
+            elif l1.next:
+                while l1.next:
+                    l1 = l1.next
+                    cur_node.next = ListNode(val = (l1.val + carry) % 10)
+                    carry = (l1.val + carry) // 10
+                    cur_node = cur_node.next
+            else:
+                while l2.next:
+                    l2 = l2.next
+                    cur_node.next = ListNode(val = (l2.val + carry) % 10)
+                    carry = (l2.val + carry) // 10
+                    cur_node = cur_node.next
+                
         if carry > 0:
             cur_node.next = ListNode(val = 1)
-            
             
         return digit_sum
             
