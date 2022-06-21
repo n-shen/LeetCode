4df9e8fe920597df31d4d789ee896170204685f5
Nicholas Shen <n@shen.dev>
2022-06-21 14:38:30 -0400

    Time: 803 ms (17.32%), Space: 25.9 MB (71.73%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        checked = {}
+        for e in nums:
+            if e in checked:
+                return True
+            checked[e] = 1
+        return False
+            
