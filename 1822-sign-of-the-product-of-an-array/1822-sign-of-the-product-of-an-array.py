975d1f2e41989a274b77d0d8086352b7c80e69ef
Nicholas Shen <n@shen.dev>
2022-06-23 01:02:24 -0400

    Time: 122 ms (11.72%), Space: 14.1 MB (35.73%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,16 +1,13 @@
 class Solution:
-    
-    def signFunc(self, x):
-        if x > 0:
-            return 1
-        elif x < 0:
-            return -1
-        else:
-            return 0
-        
     def arraySign(self, nums: List[int]) -> int:
         product = 1
-        for e in nums:
-            product *= e
         
-        return self.signFunc(product)
+        if 0 in nums:
+            return 0
+        
+        for e in nums:
+            if e < 0:
+                product *= -1
+                
+        return product
+    
