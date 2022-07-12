bc7d4253cae46a22786b8e9a8acf436991db12b8
Nicholas Shen <n@shen.dev>
2022-07-12 14:09:47 -0400

    Time: 94 ms (5.42%), Space: 13.9 MB (32.28%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -8,7 +8,7 @@ class Solution:
         flist = ListNode(0)
         
         curr = flist
-        while list1 != None and list2 != None:
+        while list1 and list2:
             if list1.val <= list2.val:
                 curr.next = list1
                 list1 = list1.next
@@ -17,10 +17,10 @@ class Solution:
                 list2 = list2.next
             curr = curr.next
         
-        if list1 == None:
-            curr.next = list2
-        else:
+        if list1:
             curr.next = list1
+        else:
+            curr.next = list2
         
         return flist.next
             
